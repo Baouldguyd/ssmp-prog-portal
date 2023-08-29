2c74ec05332303c326a1eaa0c6517844be1530d2
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2023-08-29 06:10:22 +0100

    logout

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -14,22 +14,24 @@ const Sidebar = () => {
   const Menus = [
     { title: "Dashboard", to : '/dashboard' },
     { title: "Instructors", to: '/instructors' },
+    { title: "Tasks", to: '/tasks' },
     { title: "Pending Applicants", spacing: true, to: '/pending' },
    
 
     {
       
-       title: 'Approved Students', to : '/students',
+       title: 'Enrolled Students', to : '/students',
        
   
       
     },
 
-    { title: 'Task', to:'/schedules'},
+    { title: 'Schedules', to:'/schedules'},
     { title: "Upload Events", to : '/upload' },
     
     { title: "Admin Profile", spacing: true, to : '/admin' },
     { title: "Setting", to: '/settings' },
+    { title: "LogOut", to: '/logOut' },
   ];
   return (
     <div className=" h-screen flex bg-slate-50">
@@ -39,13 +41,13 @@ const Sidebar = () => {
         } duration-300   `}
       >
         <BsArrowLeftShort
-          className={` bg-white text-slate-400 text-3xl rounded-full absolute -right-4 top-3 border border-blue-100 cursor-pointer ${
+          className={` bg-white text-slate-400 text-3xl rounded-full absolute -right-4 top-9 border border-blue-100 cursor-pointer ${
             !open && "rotate-180"
           }`}
           onClick={() => setOpen(!open)}
         />
 
-        <div className="p-1 inline-flex ">
+        <div className=" ml-2 p-1 inline-flex ">
           <AiOutlineSlack
             className={` bg-blue-500 text-white text-4xl rounded cursor-pointer block float-left duration-500 ${
               open && "rotate-[360deg]"
@@ -64,9 +66,9 @@ const Sidebar = () => {
 
 
 
-        <ul className="pt-20">
+        <ul className="pt-20 ml-2">
           {Menus.map((menu, index) => (
-            <Link>
+            <div key={index}>
               <li
                 key={index}
                 className={`text-black text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-blue-500 hover:text-white rounded-md duration-300 ${
@@ -77,7 +79,7 @@ const Sidebar = () => {
                   {menu.icon ? menu.icon : <RiDashboardFill />}
                 </span>
                 {menu.to ? (
-                <Link to={menu.to}>
+                <Link key={index} to={menu.to}>
                   <span
                     className={`text-base font-medium flex-1 duration-200 ${
                       !open && "hidden"
@@ -111,7 +113,7 @@ const Sidebar = () => {
                     className="text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 px-8 hover:bg-blue-500 rounded-md"
                   >
                     {submenuItem.to ? (
-                      <Link to={submenuItem.to}>{submenuItem.title}</Link>
+                      <Link key={index} to={submenuItem.to}>{submenuItem.title}</Link>
                     ) : (
                       submenuItem.title
                     )}
@@ -119,7 +121,7 @@ const Sidebar = () => {
                 ))}
               </ul>
               )}
-            </Link>
+            </div>
           ))}
         </ul>
       </div>
