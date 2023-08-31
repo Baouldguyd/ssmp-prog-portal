3296df4b04cf01c75cfea556806c5f203a56ff39
geezyy <xgweshyx@gmail.com>
2023-08-31 11:54:08 +0100

    Homepage updated
    updated the dashboard

---
+++ src/Pages/Popmodal.jsx
@@ -0,0 +1,20 @@
+import { QuestionCircleOutlined } from "@ant-design/icons";
+import React from "react";
+import { Popconfirm } from "antd";
+import { BsThreeDots } from "react-icons/bs";
+import styled from "@emotion/styled";
+
+const Popmodal = () => (
+  <Popconfirm
+    className=" hover:scale-150 duration-200 cursor-pointer"
+    title="Do you wish to:"
+    okText="Decline"
+    okType="danger"
+    cancelText="Approve"
+    style={{ width: "30px", backgroundColor: "black", color: "white" }}
+  >
+    <BsThreeDots />
+  </Popconfirm>
+);
+
+export default Popmodal;
